27c78ea95c333493c889c1ec088856c92f82a834
sinemoraa <jonshilin.gg@gmail.com>
2025-04-20 21:40:54 +0300

    refactor: rename

--- src/test/java/com/example/vacationcalculator/controller/VacationControllerTest.java
+++ src/test/java/com/example/vacationcalculator/controller/VacationControllerTest.java
@@ -21,7 +21,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
-class VacationControllerStandaloneTest {
+class VacationControllerTest {
 
     private MockMvc mvc;
 
